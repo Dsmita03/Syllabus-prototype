5b9cd0c20a374234633b29f67983c1a672644181
debasmitasarkar <debasmitasarkar2003@gmail.com>
2025-08-02 11:54:07 +0530

    frontend structure

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-white">
+      <h1 className="text-black text-4xl font-bold">Debasmita Sarkar</h1>
+    </main>
+  );
+}
